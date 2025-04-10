9368f5bcc5b346166deda9002e28ea8c0b06d110
Bousahla  Ahmed Abdelghani <60578625+AhmedBsl@users.noreply.github.com>
2025-04-10 12:33:12 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -26,8 +26,4 @@ const config = {
 export default config;
 
 
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+
