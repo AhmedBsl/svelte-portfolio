b9a54bf060aa3fea69566d6d9e2740a5288c3d7b
Bousahla  Ahmed Abdelghani <60578625+AhmedBsl@users.noreply.github.com>
2025-04-11 01:09:16 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 
+import './svelte-portfolio/src//app.css'
+import App from './svelte-portfolio/src//App.svelte'
+
+const app = new App({
+  target: document.getElementById('app')!,
+})
+
+export default app
