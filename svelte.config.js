66d0e4c1214f44f6906ced0b1450c72923fa2216
Bousahla  Ahmed Abdelghani <60578625+AhmedBsl@users.noreply.github.com>
2025-04-10 12:14:35 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,30 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Pour les langages comme TypeScript, SCSS, PostCSS, etc.
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null
+    }),
+    paths: {
+      base: dev ? '' : '/svelte-portfolio'
+    },
+    prerender: {
+      default: true
+    }
+  }
+};
+
+export default config;
+
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
